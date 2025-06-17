f478e21aa7aef2c63b889be377a898788177fb6a
Mitchell Morgan <mitchemorgan@gmail.com>
2025-06-17 17:54:40 -0600

    Delete Difrence

--- DeleteAtDif/DeleteAtDif.java
+++ DeleteAtDif/DeleteAtDif.java
@@ -1,5 +1,46 @@
 package DeleteAtDif;
 
 public class DeleteAtDif {
-    
+   
+    Node head;
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Keep only every 'difference'-th node
+    public void deleteAtDifference(int difference) {
+        if (head == null || difference <= 0) return;
+
+        Node current = head;
+
+        while (current != null) {
+            Node runner = current;
+            for (int i = 1; i < difference && runner.next != null; i++) {
+                runner = runner.next;
+            }
+            // connect current to the next node to keep
+            current.next = runner.next;
+            current = current.next;
+        }
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
 }
+
--- DeleteAtDif/Main.java
+++ DeleteAtDif/Main.java
@@ -1,5 +1,22 @@
 package DeleteAtDif;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        DeleteAtDif list = new DeleteAtDif();
+
+        int[] input = {1,2,3,4,5,6,7,8};
+        for (int num : input) {
+            list.insert(num);
+        }
+
+        System.out.print("Original list: ");
+        list.printList();
+
+        int difference = 3;
+        list.deleteAtDifference(difference);
+
+        System.out.print("After deleting with difference " + difference + ": ");
+        list.printList();
+    }
 }
+
