9ea63f124aa3f63d98fea5ae3e01adaee1360437
Mitchell Morgan <mitchemorgan@gmail.com>
2025-08-10 22:06:38 -0600

    bomb problem

--- Problems/FiftyNine.java
+++ Problems/FiftyNine.java
@@ -23,7 +23,102 @@
 package Problems;
 
 public class FiftyNine {
+    public int kill(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] count = new int[rows][cols];
+
+        // Calculate the number of enemies that can be killed horizontally
+        // l => r traversal
+        for (int i = 0; i < rows; ++i) {
+            int rowCount = 0;
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == 'W') { // Wall encountered, reset count
+                    rowCount = 0;
+                } else {
+                    if (grid[i][j] == 'E')
+                     rowCount++; // Enemy encountered, increment count
+                    }
+                    count[i][j] += rowCount;
+                }
+            }
+
+        // Calculate the number of enemies that can be killed vertically
+         for (int i = 0; i < rows; ++i) {
+            int rowCount = 0;
+            for (int j = cols - 1; j >= 0; --j) {
+                if (grid[i][j] == 'W') {
+                    rowCount = 0;
+                } else {
+                    if (grid[i][j] == 'E') 
+                    rowCount++;
+                }
+                count[i][j] += rowCount;
+            }
+        }
+
+        for (int j = 0; j < cols; ++j) {
+            int colCount = 0;
+            for (int i = 0; i < rows; ++i) {
+                if (grid[i][j] == 'W') {
+                    colCount = 0;
+                } else {
+                    if (grid[i][j] == 'E') 
+                    colCount++;
+                }
+                    count[i][j] += colCount;
+                }
+        }
+
+       for (int j = 0; j < cols; ++j) {
+            int colCount = 0;
+            for (int i = rows - 1; i >= 0; --i) {
+                if (grid[i][j] == 'W') {
+                    colCount = 0;
+                } else {
+                    if (grid[i][j] == 'E')
+                    colCount++;
+                }
+                count[i][j] += colCount;
+            }
+        }
+
+        // Find the best empty cell
+        int maxKills = 0;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == '0') { // Empty cell, potential for a bomb
+                    maxKills = Math.max(maxKills, count[i][j]);
+                }
+            }
+        }
+        return maxKills;
+    }
+
+
+     public static void main(String[] args) {
+        FiftyNine solver = new FiftyNine();
+
     
+        char[][] ex1 = {
+            {'0','E','0','0'},
+            {'E','0','W','E'},
+            {'0','E','0','0'}
+        };
+        int out1 = solver.kill(ex1);
+        System.out.println("Example1 output: " + out1);
+        assert out1 == 3 : "Example 1 should be 3";
+
+ 
+        char[][] ex2 = {
+            {'0','E','0','0'},
+            {'E','E','W','E'},
+            {'0','E','0','0'}
+        };
+        int out2 = solver.kill(ex2);
+        System.out.println("Example2 output: " + out2);
+        assert out2 == 2 : "Example 2 should be 2";
+    }
 }
 
-// 27 28 29 30 & 59
+// 27 28 29 30 
